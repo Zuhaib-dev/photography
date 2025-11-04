e44df4d7418ff94fc11c10fd3bafa126f7136ca8
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-11-04 22:24:35 +0530

    Ready to Deploy

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,7 +5,7 @@ const SocialIcons = () => {
     <div className="flex gap-2 border-neutral-800 border max-w-max p-1 rounded-full items-center">
       {socialLinks.map((item, index) => (
         <a
-          href="#"
+          href="https://www.zuhaibrashid.com/"
           target="_blank"
           className="w-10 h-10 bg-neutral-800 flex items-center justify-center rounded-full aspect-square hover:bg-neutral-700 transition-colors"
           key={index}
